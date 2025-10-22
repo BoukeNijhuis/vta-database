8a7ba7636ed3f248af2d9362e772c306ca7a5a61
Titus Nachbauer <titus.nachbauer@nn-group.com>
2025-10-22 12:41:37 +0200

    Expand tests with content and edge cases

--- src/test/java/org/example/vtadatabase/VTADatabaseTest.java
+++ src/test/java/org/example/vtadatabase/VTADatabaseTest.java
@@ -28,11 +28,57 @@ public class VTADatabaseTest {
     }
 
     @Test
+    public void getClientById_returnsCorrectClientData() {
+        given()
+                .when().get("/clients/1")
+                .then()
+                .statusCode(200)
+                .contentType("application/json")
+                .body("id", equalTo(1))
+                .body("name", equalTo("Jan Jansen"))
+                .body("email", equalTo("jan.jansen@gmail.com"));
+    }
+
+    @Test
     public void getClientById_whenClientDoesNotExist_returns404() {
         given()
                 .when().get("/clients/999999")
                 .then()
                 .statusCode(404);
+    }
+
+    @Test
+    public void getClientById_withNegativeId_returns404() {
+        given()
+                .when().get("/clients/-1")
+                .then()
+                .statusCode(404);
+    }
+
+    @Test
+    public void getClientById_withZeroId_returns404() {
+        given()
+                .when().get("/clients/0")
+                .then()
+                .statusCode(404);
+    }
+
+    @Test
+    public void getAllClients_returnsCorrectContentType() {
+        given()
+                .when().get("/clients")
+                .then()
+                .statusCode(200)
+                .contentType("application/json");
+    }
+
+    @Test
+    public void getAllClients_containsExpectedClient() {
+        given()
+                .when().get("/clients")
+                .then()
+                .statusCode(200)
+                .body("find { it.name == 'Jan Jansen' }.email", equalTo("jan.jansen@gmail.com"));
     }
 }
 
